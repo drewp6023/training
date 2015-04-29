d6ea47723a04db9a5fe15072331d09b379bb4908
Drew <pickerdr@gmail.com>
2015-04-28 18:31:07 -0600

    Grunt server

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,38 @@ module.exports = function(grunt) {
 				devDependencies: true,
 				exclude: ['/jquery/', '/bootstrap/dist/js/']
 			}
-		},	
+		},
+
+		watch: {
+		    options: {
+		        livereload: true
+		    },
+		    taskName: {    // You need a task, can be any string
+		        files: [   // Files to livereload on
+		            "app/**/*.js",
+		            "index.html",
+		            "app/**/*.html",
+		            "app/styles/*.css"
+		        ]
+		    }
+		},
+
+		connect: {
+		    server: {
+		        options: {
+		            port: 8080,
+		            livereload: true
+		        }
+		    }
+		},
+
+		// grunt-open will open your browser at the project's URL
+		open: {
+		  all: {
+		    // Gets the port from the connect configuration
+		    path: 'http://localhost:8080/#/>'
+		  }
+		},
 
 		//JSHint
 		jshint: {
@@ -26,4 +57,5 @@ module.exports = function(grunt) {
 	});
 
 	grunt.registerTask('default', []);
+	grunt.registerTask('server', ['open', 'connect', 'watch']);
 };
